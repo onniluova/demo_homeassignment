da07e5a11cde4b07109b0bd66f8bb4960176e681
onboni <onni.luova@gmail.com>
2024-10-28 11:46:42 +0200

    init

--- src/main/java/org/example/demo_homeassignment/HelloController.java
+++ src/main/java/org/example/demo_homeassignment/HelloController.java
@@ -1,14 +1,46 @@
 package org.example.demo_homeassignment;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button1;
+
+    public void setEN(ActionEvent actionEvent) {
+        loadView(new Locale("en"));
+    }
+
+    public void setIR(ActionEvent actionEvent) {
+        loadView(new Locale("IR"));
+    }
+
+    public void setJP(ActionEvent actionEvent) {
+        loadView(new Locale("JP"));
+    }
+
+    public void loadView(Locale locale) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("bundle", locale));
+        try {
+            Object root = fxmlLoader.load();
+            Stage s = (Stage) button1.getScene().getWindow();
+            s.setScene(new Scene((Parent) root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void main() {
     }
 }
